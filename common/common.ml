bbcdb24f4456fd2b6b1ff3d4228e33dc9e9c1a9d
Nathan Farlow <natethegreatest11@gmail.com>
2024-12-02 20:24:11 -0500

    Fix zip next

--- common/common.ml
+++ common/common.ml
@@ -9,18 +9,31 @@ let integer = take_while1 Char.is_digit >>| Int.of_string
 let eol = string "\n" >>| ignore
 let exec parser s = parse_string ~consume:All parser s |> Result.ok_or_failwith
 
-let rec zip_next = function
-  | [] -> []
-  | [ _ ] -> []
-  | x :: y :: xs -> (x, y) :: zip_next (y :: xs)
+let zip_next l =
+  let rec aux acc = function
+    | [] | [ _ ] -> acc
+    | x :: (y :: _ as xs) -> aux ((x, y) :: acc) xs
+  in
+  aux [] l |> List.rev
+
+let%expect_test "zip_next" =
+  zip_next [ 1; 2; 3; 4 ] |> [%sexp_of: (int * int) list] |> print_s;
+  [%expect {| ((1 2) (2 3) (3 4)) |}]
 
 let sum ~f = List.sum (module Int) ~f
-let print_int = printf "%d"
+let print_int = printf "%d\n"
 
 let run_with_input_file ~part1 ~part2 ~parse =
   Command.basic ~summary:"Advent of code"
     (let%map_open.Command f =
-       choose_one ~if_nothing_chosen:Raise
+       choose_one
+         ~if_nothing_chosen:
+           (Default_to
+              (fun s ->
+                print_endline "Part 1:";
+                part1 s;
+                print_endline "Part 2:";
+                part2 s))
          [
            flag "-part1" no_arg ~doc:"Run part 1"
            |> map ~f:(fun b -> Option.some_if b part1);
