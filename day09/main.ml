9702162c60175cb6a4a9f8f146c1433aed76efc2
Nathan Farlow <natethegreatest11@gmail.com>
2024-12-10 23:32:15 -0500

    Cleanup

--- day09/main.ml
+++ day09/main.ml
@@ -1,12 +1,7 @@
 open! Core
 open! Common
 
-let part1 input =
-  let arr =
-    List.concat_mapi input ~f:(fun id (file, hole) ->
-        List.init file ~f:(fun _ -> Some id) @ List.init hole ~f:(fun _ -> None))
-    |> Array.of_list
-  in
+let part1 (_, arr) =
   let rec aux acc left right =
     if left > right then acc
     else
@@ -17,41 +12,42 @@ let part1 input =
   in
   aux 0 0 (Array.length arr - 1) |> print_int
 
-let part2 input =
-  let arr =
-    List.concat_mapi input ~f:(fun id (file, hole) ->
-        List.init file ~f:(fun _ -> Some id) @ List.init hole ~f:(fun _ -> None))
-    |> Array.of_list
-  in
+let part2 (pairs, arr) =
   let rec copy cur left right =
-    match (arr.(left), arr.(right)) with
-    | _, r when not @@ [%equal: int option] r cur -> true
-    | l, _ when [%equal: int option] l cur -> false
-    | Some _, _ -> false
-    | None, _ ->
-        if copy cur (left + 1) (right - 1) then (
-          arr.(left) <- cur;
-          arr.(right) <- None;
-          true)
-        else false
+    if left > right then false
+    else
+      match (arr.(left), arr.(right)) with
+      | _, r when not @@ [%equal: int option] r cur -> true
+      | Some _, _ -> false
+      | None, _ ->
+          if copy cur (left + 1) (right - 1) then (
+            arr.(left) <- cur;
+            arr.(right) <- None;
+            true)
+          else false
   in
-  List.rev input
+  List.rev pairs
   |> List.fold
        ~init:(Array.length arr - 1)
-       ~f:(fun right (files, hole) ->
-         let right = right - hole in
+       ~f:(fun right (files, holes) ->
+         let right = right - holes in
          for i = 0 to right do
            copy arr.(right) i right |> ignore
          done;
          right - files)
   |> ignore;
-  Array.foldi arr ~init:0 ~f:(fun i acc a ->
-      acc + (i * Option.value a ~default:0))
+  Array.fold arr ~init:0 ~f:(fun acc elem -> acc + Option.value elem ~default:0)
   |> print_int
 
-let parse =
+let parse s =
   let open Angstrom in
   let pair = lift2 Tuple2.create digit (digit <|> return 0) in
-  many pair |> exec ~consume:Prefix
+  let pairs = exec (many pair) s ~consume:Prefix in
+  let arr =
+    List.concat_mapi pairs ~f:(fun id (file, hole) ->
+        List.init file ~f:(fun _ -> Some id) @ List.init hole ~f:(fun _ -> None))
+    |> Array.of_list
+  in
+  (pairs, arr)
 
 let () = run_with_input_file ~part1 ~part2 ~parse
